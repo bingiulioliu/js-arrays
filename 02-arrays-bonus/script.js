9a332b77a9023f00942341659f4fc1ffad329669
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-03-25 15:32:54 +0100

    DONE es 2.1 e 2.2

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,11 +10,19 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+const reversedTeachers = teachers.reverse();
+console.log('2.1 ' + reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = [];
+  for (let i=0; i < teachers.length; i++) {
+    const currentTeacher = teachers[i];
+    if (currentTeacher.length >= 5) {
+      longNames.push(currentTeacher);
+    }
+  }
+console.log('2.2 ' + longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
